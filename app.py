aa71b541c443d156496374d22d94e5528f9a8f81
Vicknavel <74656818+vicknavelcode@users.noreply.github.com>
2020-11-21 16:31:38 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+#Simple Python Calculator
+#I know that everyone can do this so please bare me
+firstdigit=int(input(" Enter Your First Number : "))
+seconddigit=int(input('Enter Your Second Number : '))
+sum=firstdigit+seconddigit
+print(sum)
